56c9028e26401bc26505dae7ae8d85fc4a9085fa
Abinoveramesh20 <abinove1999@gmail.com>
2024-05-21 13:17:03 +0530

    fix axios

--- eatzilla-web-api-main/vendor/composer/installed.php
+++ eatzilla-web-api-main/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '045150645ce66cac3a10fb50a2ac50d15b2e0a9e',
+        'reference' => '7f16cda66be83df835302d3697a6343625061b6f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '045150645ce66cac3a10fb50a2ac50d15b2e0a9e',
+            'reference' => '7f16cda66be83df835302d3697a6343625061b6f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
